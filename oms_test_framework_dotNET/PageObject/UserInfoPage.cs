4b0f0662f5861b4f7a7586c62f1448a065251e08
rkliuha <ngnaga2013@yandex.ua>
2016-10-17 19:55:08 +0300

    merge

--- oms_test_framework_dotNET/PageObject/UserInfoPage.cs
+++ oms_test_framework_dotNET/PageObject/UserInfoPage.cs
@@ -13,6 +13,22 @@ namespace oms_test_framework_dotNET.PageObject
         [FindsBy(How = How.XPath, Using = "//ul[@id='nav']/descendant::a[@href='itemManagement.htm']")]
         public IWebElement ItemManagementLink { get; set; }
 
+        [FindsBy(How = How.XPath, Using = "//ul[@id='nav']/li[2]/a")]
+        public static IWebElement UserInfoLink { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//ul[@id='nav']/li[1]/a")]
+        public IWebElement CustomerOrderingLink { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//ul[@id='nav']/li[1]/a")]
+        public IWebElement AdministrationLink { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//ul[@id='nav']/li[1]/a")]
+        public IWebElement MerchandiserOrderingLink { get; set; }
+
+        // UserInfoFieldSet is unique element on UserInfoPage
+        [FindsBy(How = How.XPath, Using = "//div[@id='content']//legend")]
+        public IWebElement UserInfoFieldSet { get; set; }
+
         public UserInfoPage(IWebDriver driver) : base(driver)
         {
         }
@@ -22,6 +38,23 @@ namespace oms_test_framework_dotNET.PageObject
             ItemManagementLink.Click();
             return new ItemManagementPage(Driver);
         }
-    }
 
+        public CustomerOrderingPage ClickCustomerOrdetingLink()
+        {
+            CustomerOrderingLink.Click();
+            return new CustomerOrderingPage(Driver);
+        }
+
+        public AdministrationPage ClickAdministrationLink()
+        {
+            AdministrationLink.Click();
+            return new AdministrationPage(Driver);
+        }
+
+        public MerchandiserOrderingPage ClickMerchandiserOrderingLink()
+        {
+            MerchandiserOrderingLink.Click();
+            return new MerchandiserOrderingPage(Driver);
+        }
+    }
 }
