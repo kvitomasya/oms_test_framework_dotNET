a91131190feb4e30fd0e3ec446d44eac36ca06f3
DmytroKuzin <dimabeton@mail.ru>
2016-10-18 16:40:38 +0300

    Ticket 105

---
+++ oms_test_framework_dotNET/PageObject/CreateUserPage.cs
@@ -0,0 +1,116 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+
+namespace oms_test_framework_dotNET.PageObject
+{
+    public class CreateUserPage : PageObject
+    {
+        //Unique CreateUserPage Web Element
+        [FindsBy(How = How.XPath, Using = "//*[@id='userModel']/table/tbody/tr[1]/td[1]")]
+        public IWebElement LoginNameLabel { get; set; }
+
+        [FindsBy(How = How.Id, Using = "login")]
+        public IWebElement LoginInput { get; set; }
+
+        [FindsBy(How = How.Id, Using = "firstName")]
+        public IWebElement FirstNameInput { get; set; }
+
+        [FindsBy(How = How.Id, Using = "lastName")]
+        public IWebElement LastNameInput { get; set; }
+
+        [FindsBy(How = How.Id, Using = "password")]
+        public IWebElement PasswordInput { get; set; }
+
+        [FindsBy(How = How.Id, Using = "confirmPassword")]
+        public IWebElement ConfirmPasswordInput { get; set; }
+
+        [FindsBy(How = How.Id, Using = "email")]
+        public IWebElement EmailInput { get; set; }
+
+        [FindsBy(How = How.Id, Using = "name")]
+        public IWebElement ProductNameInput { get; set; }
+
+        [FindsBy(How = How.Id, Using = "regionID")]
+        public IWebElement RegionIdCheckBox { get; set; }
+
+        [FindsBy(How = How.Id, Using = "roleID")]
+        public IWebElement RoleCheckBox { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='userModel']/input[4]")]
+        public IWebElement CreateButton { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='userModel']/input[5]")]
+        public IWebElement CancelButton { get; set; }
+
+        public CreateUserPage(IWebDriver driver) : base(driver)
+        {
+        }
+
+        public CreateUserPage FillLoginField(string login)
+        {
+            LoginInput.SendKeys(login);
+            return this;
+        }
+
+        public CreateUserPage FillFirstNameField(string firstName)
+        {
+            FirstNameInput.SendKeys(firstName);
+            return this;
+        }
+
+        public CreateUserPage FillLastNameField(string lastName)
+        {
+            LastNameInput.SendKeys(lastName);
+            return this;
+        }
+
+        public CreateUserPage FillPasswordField(string password)
+        {
+            PasswordInput.SendKeys(password);
+            return this;
+        }
+
+        public CreateUserPage FillConfirmPasswordField(string confirmPassword)
+        {
+            ConfirmPasswordInput.SendKeys(confirmPassword);
+            return this;
+        }
+
+        public CreateUserPage FillEmailField(string email)
+        {
+            EmailInput.SendKeys(email);
+            return this;
+        }
+
+        public CreateUserPage ChooseRegion(string region)
+        {
+            RegionIdCheckBox.SendKeys(region);
+            return this;
+        }
+
+        public CreateUserPage ChooseRole(string role)
+        {
+            RoleCheckBox.SendKeys(role);
+            return this;
+        }
+
+        public AdministrationPage ClickCreateButton()
+        {
+            CreateButton.Click();
+            return new AdministrationPage(Driver);
+        }
+
+        public CreateUserPage ClickCancelButton()
+        {
+            CancelButton.Click();
+            return this;
+        }
+    }
+}
---
+++ oms_test_framework_dotNET/Tests/Administrator/CreateUserTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using oms_test_framework_dotNET.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace oms_test_framework_dotNET.Tests
+{
+    [TestClass]
+    public class CreateUserTest : TestRunner
+    {
+        [TestMethod]
+        public void testCreateNewUserAbility()
+        {
+            const String AdminLogin = "iva";
+            const String AdminPassword = "qwerty";
+
+            administrationPage = logInPage
+                     .LogInAs(AdminLogin, AdminPassword)
+                     .ClickAdministrationLink();
+
+            Assert
+                .IsTrue(administrationPage.FoundUsersTextLabel.Displayed,
+                "The Admin page should be displayed!");
+
+            createUserPage = administrationPage.ClickCreateNewUser();
+
+            Assert
+                .IsTrue(createUserPage.LoginNameLabel.Displayed,
+                "The CreateNewUserPage should be displayed!");
+
+            createUserPage
+            .FillLoginField("StefDevis")
+            .FillFirstNameField("Stef")
+            .FillLastNameField("Devis")
+            .FillPasswordField("qwerty")
+            .FillConfirmPasswordField("qwerty")
+            .FillEmailField("StefDevis@gmail.com")
+            .ChooseRegion("North")
+            .ChooseRole("Customer");
+
+            createUserPage.ClickCreateButton();
+
+            Assert
+                .IsTrue(administrationPage.FoundUsersTextLabel.Displayed,
+                "The Admin page should be displayed!");
+
+            administrationPage
+                .FillFieldFilter("Login")
+                .FillConditionFilter("equal")
+                .FillSearchInputField("StefDevis")
+                .ClickSearchButton();
+
+            Assert
+                .AreEqual("StefDevis", administrationPage.LogInFirstCellLink.Text,
+                "As a result of the search should be a user with the specified login");
+
+            administrationPage.DeleteUserByLogIn("StefDevis");
+        }
+    }
+}
+
