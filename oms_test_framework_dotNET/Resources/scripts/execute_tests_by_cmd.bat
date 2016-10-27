1deafee7e0a6243c54490942c06ba3e3763ebbcc
Kostyantin Sichenko <kostyantins@mail.ru>
2016-10-27 21:58:16 +0300

    Update execute_tests_by_cmd.bat

--- oms_test_framework_dotNET/Resources/scripts/execute_tests_by_cmd.bat
+++ oms_test_framework_dotNET/Resources/scripts/execute_tests_by_cmd.bat
@@ -1,7 +1,9 @@
 cd ..\..\bin\Debug\
 
-IF "%1"=="" ( 
+IF "%1"=="" (
+::running all tests, without parameters
 vstest.console.exe oms_test_framework_dotNET.dll /tests:EditOrderTest,DeleteOrderTest,LogOutTest,SwitchTabsTest,PositiveLogInTest
- ) ELSE ( 
+ ) ELSE (
+ ::running just test sent to parameter
  vstest.console.exe oms_test_framework_dotNET.dll /tests:%1
  )
